2b0d797306369e7452b44bb80d71fe1325ef4462
javier <devjavicont@gmail.com>
2024-05-29 10:22:41 -0400

    backend: cambiar el nombre de la base de datos (mongodb)

--- prueba-desocom/backend/src/config.ts
+++ prueba-desocom/backend/src/config.ts
@@ -2,7 +2,7 @@ import { config } from "dotenv";
 config();
 
 export const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost:27017/test-online-shop";
+  process.env.MONGODB_URI || "mongodb://localhost:27017/desocom-backend";
 export const PORT = process.env.PORT || "3060";
 
 export const JWT_KEY = process.env.JWT_KEY || "secret-key";
